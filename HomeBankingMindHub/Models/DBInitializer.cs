120f4029b9cfeea227953fbbc7a692b7afb1c172
vcoronado2014 <vcoronado.alarcon@gmail.com>
2022-12-05 10:14:19 -0300

    todo

---
+++ HomeBankingMindHub/Models/DBInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace HomeBankingMindHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"}
+                };
+
+                foreach (Client client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+
+                //guardamos
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ HomeBankingMindHub/Models/HomeBankingContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace HomeBankingMindHub.Models
+{
+    public class HomeBankingContext : DbContext
+    {
+        public HomeBankingContext(DbContextOptions<HomeBankingContext> options) : base(options) { }
+
+        //dbsets
+        public DbSet<Client> Clients { get; set; }
+    }
+}
